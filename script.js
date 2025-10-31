7b213d9a5d19f7fe2919ac6968e89766fc1c2962
Amir Hooka <amirhooka@Amirs-MacBook-Pro.local>
2025-11-01 01:45:53 +0330

    add a GUI for rock paper scisssors game.

--- script.js
+++ script.js
@@ -1,82 +1,139 @@
-
 function playGame() {
-    let computerScore = 0;
-    let humanScore = 0; 
-    let round = 0;
-    function getComputerChoice() {
-        let rndNum = Math.random();
-        let computerChoice;
-        if (rndNum <= 0.33) {
-            computerChoice = "rock"
-        } else if (rndNum <= 0.66) {
-            computerChoice = "paper";
-        } else {
-            computerChoice = "scissors";
-        }
-        console.log("Computer chose " + computerChoice);
-        return computerChoice;
+  let computerScore = 0;
+  let userScore = 0;
+  let round = 0;
+  function getComputerChoice() {
+    let rndNum = Math.random();
+    let computerChoice;
+    if (rndNum <= 0.33) {
+      computerChoice = "rock";
+    } else if (rndNum <= 0.66) {
+      computerChoice = "paper";
+    } else {
+      computerChoice = "scissors";
     }
-    function getHumanChoice() {
-        let humanChoice = prompt("Choose").toLowerCase();
-        console.log("Human chose " + humanChoice);
-        return humanChoice;
+    
+    console.log("Computer chose " + computerChoice);
+    return computerChoice;
+  }
+  // function getUserChoice() {
+  //   let userChoice = prompt("Choose").toLowerCase();
+  //   console.log("user chose " + userChoice);
+  //   return userChoice;
+  // }
+  
+  function playRound(userChoice, computerChoice) {
+    if (!game) {
+      return;
     }
-    function playRound(humanChoice , computerChoice) {
-        if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore ++;
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore ++;
-        } else if (humanChoice === "rock" && computerChoice === "rock") {
-            console.log("It's a tie!");
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore ++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore ++;
-        } else if (humanChoice === "paper" && computerChoice === "paper") {
-            console.log("It's a tie!");
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            computerScore ++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore ++;
-        } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-            console.log("It's a tie!");
-        }
+
+    if (userChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+      userScore++;
+    } else if (userChoice === "rock" && computerChoice === "paper") {
+      computerScore++;
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+      userScore++;
+    } else if (userChoice === "paper" && computerChoice === "scissors") {
+      computerScore++;
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+      userScore++;
+    } else if (userChoice === "scissors" && computerChoice === "rock") {
+      computerScore++;
     }
-    //round 1 
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    
-    playRound(humanChoice , computerChoice);
 
-    //round 2
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    
+    round += 1;
 
-    playRound(humanChoice , computerChoice);
+    if (round >= maxRounds) {
+      game = false;
+    }
+  }
+  let game = true;
+  const maxRounds = 5;
+  const rockBtn = document.querySelector(".playerBtn#rock");
+  const paperBtn = document.querySelector(".playerBtn#paper");
+  const scissorsBtn = document.querySelector(".playerBtn#scissors");
 
-    //round 3
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    
+  const scoreDisplay = document.querySelector("#scoreDisplay");
+  const winnerP = document.querySelector("#winnerP");
 
-    playRound(humanChoice , computerChoice);
 
-    //round 4
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    
+  rockBtn.addEventListener("click", function (e) {
+    playRound(e.target.id , getComputerChoice());
+    scoreDisplay.textContent = "User: " + userScore + " Computer: " + computerScore;
+    if(!game) {
+      if (userScore > computerScore) {
+        winnerP.textContent = "USER WON!";
+      } else if (userScore < computerScore) {
+        winnerP.textContent = "COMPUTER WON!";
+      } else {
+        winnerP.textContent = "THE GAME WAS A TIE";
+      }
+    }
+  });
+  paperBtn.addEventListener("click", function (e) {
+    playRound(e.target.id , getComputerChoice());
+    scoreDisplay.textContent = "User: " + userScore + " Computer: " + computerScore;
+    if(!game) {
+      if (userScore > computerScore) {
+        winnerP.textContent = "USER WON!";
+      } else if (userScore < computerScore) {
+        winnerP.textContent = "COMPUTER WON!";
+      } else {
+        winnerP.textContent = "THE GAME WAS A TIE";
+      }
+    }
+  });
+  scissorsBtn.addEventListener("click", function (e) {
+    playRound(e.target.id , getComputerChoice());
+    scoreDisplay.textContent = "User: " + userScore + " Computer: " + computerScore;
+    if(!game) {
+      if (userScore > computerScore) {
+        winnerP.textContent = "USER WON!";
+      } else if (userScore < computerScore) {
+        winnerP.textContent = "COMPUTER WON!";
+      } else {
+        winnerP.textContent = "THE GAME WAS A TIE";
+      }
+    }
+  });
 
-    playRound(humanChoice , computerChoice);
 
-    //round 5
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    
 
-    playRound(humanChoice , computerChoice);
 
-    console.log("Player : " + humanScore + " Computer : " + computerScore);
+//   //round 1
+//   let userChoice = getUserChoice();
+//   let computerChoice = getComputerChoice();
+
+//   playRound(userChoice, computerChoice);
+
+//   //round 2
+//   userChoice = getUserChoice();
+//   computerChoice = getComputerChoice();
+
+//   playRound(userChoice, computerChoice);
+
+//   //round 3
+//   userChoice = getUserChoice();
+//   computerChoice = getComputerChoice();
+
+//   playRound(userChoice, computerChoice);
+
+//   //round 4
+//   userChoice = getUserChoice();
+//   computerChoice = getComputerChoice();
+
+//   playRound(userChoice, computerChoice);
+
+//   //round 5
+//   userChoice = getUserChoice();
+//   computerChoice = getComputerChoice();
+
+//   playRound(userChoice, computerChoice);
+
+//   console.log("Player : " + userScore + " Computer : " + computerScore);
+
 }
 
 playGame();
